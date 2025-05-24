6096334c73ec5be3da11c1895d36d6711901994f
Bruno Netto <brunonettomac@outlook.com>
2025-05-24 20:12:56 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
-  return <h1>HomePage</h1>
+  return (
+    <div>
+      <h1>HomePage</h1>
+
+      <Button>Olá</Button>
+    </div>
+  )
 }
