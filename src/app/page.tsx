55e6334f58f99ff0f34d6855926dc66e124e6721
Bruno Netto <brunonettomac@outlook.com>
2025-05-25 17:16:33 -0300

    feat: add booking item component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import BarbershopItem from "@/components/barbershop-item"
+import BookingItem from "@/components/booking-item"
 import Header from "@/components/header"
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { quickSearchOptions } from "@/constants/search"
 import { db } from "@/lib/prisma"
-import { EyeIcon, Search } from "lucide-react"
+import { Search } from "lucide-react"
 import Image from "next/image"
 
 const Home = async () => {
@@ -33,30 +33,17 @@ const Home = async () => {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" alt="cabelo" width={16} height={16} />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg"
-              alt="acabamento"
-              width={16}
-              height={16}
-            />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" alt="barba" width={16} height={16} />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <EyeIcon size={16} />
-            Sobrancelha
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} variant="secondary" className="gap-2">
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -68,29 +55,7 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Maio</p>
-              <p className="text-2xl">25</p>
-              <p className="text-sm">14:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
---
+++ src/constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOptions {
+  title: string
+  imageUrl: string
+}
+
+export const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    title: "Cabelo",
+    imageUrl: "/cabelo.svg",
+  },
+  {
+    title: "Acabamento",
+    imageUrl: "/acabamento.svg",
+  },
+  {
+    title: "Barba",
+    imageUrl: "/barba.svg",
+  },
+  {
+    title: "Sobrancelha",
+    imageUrl: "/sobrancelha.svg",
+  },
+  {
+    title: "Massagem",
+    imageUrl: "/massagem.svg",
+  },
+  {
+    title: "Hidratação",
+    imageUrl: "/hidratacao.svg",
+  },
+]
