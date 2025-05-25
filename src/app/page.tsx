29fbe4ce9f32fe7469c67dcd3cf2fc3f3fa47214
Bruno Netto <brunonettomac@outlook.com>
2025-05-25 10:55:57 -0300

    feat: add Header component and Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <>
+      <Header />
 
-      <Button>Olá</Button>
-    </div>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Bruno!</h2>
+        <p className="">Domingo, 25 de maio</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <Search />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende com os melhores com FSW barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </>
   )
 }
